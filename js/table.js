450399860817de64b0c6d9d7aa0f19cdb2aa5302
hosoboso <139880091+hosoboso@users.noreply.github.com>
2024-06-09 09:47:54 +0900

    Update table.js
    ラジオボタンでタブとカンマ区切り分け

--- js/table.js
+++ js/table.js
@@ -2,18 +2,20 @@ function func1() {
 	let gettext1 = document.getElementById("textarea1").value;
 	let gettext2 = document.getElementById("textarea2");
 
+	if (document.getElementById("num2").checked) {
+		gettext1 = gettext1.replaceAll(",", "\t");
+	}
+
 	//textarea1がカラの時は注意書き、それ以外は多次元配列arr2を作る
 	if (gettext1.length == 0) {
 		document.getElementById("view1").innerHTML = "テキストを入力してください";
 	} else {
+		//改行で分割（split("\n")）
 		arr1 = [];
 		arr1 = gettext1.split("\n");
 		arr2 = [];
 		arr2 = arr1.map((item1) => item1.split("\t"));
 	}
-	//改行で分割（split("\n")の部分）、
-	//続いて作った配列の中身をtab部分で分割（split("\t")）して多次元配列生成
-	//map() はすべての配列アイテムごとにコールバック関数 callback を実行し、返値からなる新しい配列を返す
 
 	let tablerow = "";	//1行分のタグ
 	let tabletext = "";	//テーブルタグ全体
@@ -45,8 +47,11 @@ function func1() {
 function func2() {
 	//textarea2の値を取得
 	let textar = document.getElementById("textarea2").value;
-	//クリップボードに書き込む
-	navigator.clipboard.writeText(textar);
-	document.getElementById("view2").innerHTML = "クリップボードにコピーしました";
-
+	if (textar.length == 0) {
+		document.getElementById("view2").innerHTML = "コピーするテキストがありません";
+	} else {
+		//クリップボードに書き込む
+		navigator.clipboard.writeText(textar);
+		document.getElementById("view2").innerHTML = "クリップボードにコピーしました";
+	}
 }
